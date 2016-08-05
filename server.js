9a18294eded6572caba4d41ef3b08758aae52c85
Gregory Bird <Gregory Bird>
2016-08-04 19:44:19 -0600

    prep repo for heroku

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ var express = require('express');
 // Create the app
 var app = express();
 
+const PORT = process.env.port || 3000;
+
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hotname + req.url);
+  }
+});
+
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-  console.log('Express Server is up on port 3000');
+app.listen(PORT, function() {
+  console.log('Express Server is up on port ' + PORT);
 });
