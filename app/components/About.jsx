7307e2ef2af62bef10c23e9d1e207ee1c28a32e2
Gregory Bird <Gregory Bird>
2016-08-10 17:34:22 -0600

    added style to about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,8 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About page.</p>
+      <h1 className="text-center">About</h1>
+      <p>Do you need to know the weather where you are right now? Just search the city where you're at and we can tell you the exact temperature.</p>
     </div>
   );
 };
