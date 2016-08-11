9f69c933489b4d3be42460b53d20eda95140a6ee
Gregory Bird <Gregory Bird>
2016-08-10 21:07:37 -0600

    added custom styling

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>Do you need to know the weather where you are right now? Just search the city where you're at and we can tell you the exact temperature.</p>
     </div>
   );
